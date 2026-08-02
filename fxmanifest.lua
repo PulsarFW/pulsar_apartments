ed45082af3ef5ba9461e094349c250b5669da85e
Artmines <arturowells@gmail.com>
2026-08-02 11:38:45 -0230

    feat(version check + license): version and license

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,6 +7,9 @@ author 'Artmines - maintained for Pulsar Framework'
 url 'https://pulsarframe.work'
 version 'v1.0.0'
 
+version_check 'yes'
+github 'https://github.com/PulsarFW/pulsar_apartments'
+
 client_script '@pulsar_core/components/cl_error.lua'
 shared_script '@pulsar_core/core/sh_pulsar.lua'
 client_script '@pulsar_pwnzor/client/check.lua'
